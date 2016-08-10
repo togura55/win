a73980902924acee9eec3434d07d75dfa72007df
Tsuyoshi <togura55@gmail.com>
2016-08-10 14:18:13 +0900

    MyUiSig
    Custom UI for Signature Sample

---
+++ MyUiSig/MyUiSig/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MyUiSig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSign
+            // 
+            this.btnSign.Location = new System.Drawing.Point(270, 80);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(75, 23);
+            this.btnSign.TabIndex = 0;
+            this.btnSign.Text = "button1";
+            this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 241);
+            this.Controls.Add(this.btnSign);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSign;
+    }
+}
+
---
+++ MyUiSig/MyUiSig/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUiSig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            ShowCustomSignWindow();
+        }
+
+        private void ShowCustomSignWindow()
+        {
+            bool success = wizCtl.PadConnect();
+            if (success)
+            {
+                if (wizCtl.PadWidth != 800)
+                {
+                    success = false;
+                }
+            }
+
+            if (success != true)
+            {
+                MessageBox.Show("This demo is only for STU-530");
+                return;
+            }
+
+            wizCtl.Reset();
+
+            wizCtl.AddObject(ObjectType.ObjectImage, "", "left", "top", "signature_color.png", null);
+            wizCtl.AddObject(ObjectType.ObjectImage, "OK", "550", "384", "accept_button.png", null);
+            wizCtl.AddObject(ObjectType.ObjectImage, "Cancel", "550", "300", "cancel_button.png", null);
+            wizCtl.AddObject(ObjectType.ObjectText, "who", "30", "460", txtName.Text, null);
+            wizCtl.AddObject(ObjectType.ObjectText, "why", "334", "460", "Acknowledged and confirmed", null);
+            wizCtl.AddObject(ObjectType.ObjectSignature, "signature", 0, 0, sigObj, null);
+
+
+            callback.EventHandler = new WizardCallback.Handler(button_handler);
+            wizCtl.SetEventHandler(callback);
+
+
+            wizCtl.Display();
+        }
+    }
+}
