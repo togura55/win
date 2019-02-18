d87879669c839eb4e967c81421cc709d7a720041
togura55 <togura55@gmail.com>
2019-02-18 18:25:22 +0900

    DPMC
    1st checkin

---
+++ DrawPointsMultiCanvas/DrawPointsMultiCanvas/DrawPoint.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawPointsMultiCanvas
+{
+    class DrawPoint
+    {
+        public DrawPoint()
+        {
+
+        }
+
+        public void Start()
+        {
+            // generate a timer thread   
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        private void TimerThread()
+        {
+
+        }
+
+        //List<Circle> circle;
+        //Paint paint;
+        //Point size;
+        //static final int BALL_NUMBER = 500;
+
+        //public RandomView(Context context)
+        //{
+        //    super(context);
+
+        //    circle = new ArrayList<>();                 //円のリストを作成します
+        //    Random rnd = new Random();                  //乱数を発生させます
+        //    paint = new Paint();
+
+        //    for (int i = 0; i < BALL_NUMBER; i++)
+        //    {
+        //        Circle c = new Circle();
+        //        c.x = rnd.nextInt(1440);        //ランダムなX座標の取得
+        //        c.y = rnd.nextInt(2560);        //ランダムなY座標の取得
+        //        c.r = rnd.nextInt(256);         //ランダムな赤要素の取得
+        //        c.g = rnd.nextInt(256);         //ランダムな緑要素の取得
+        //        c.b = rnd.nextInt(256);         //ランダムな青要素の取得
+        //        c.s = rnd.nextInt(50);          //ランダムな円の大きさを取得
+        //        circle.add(c);
+        //    }
+        //}
+        //protected void onDraw(Canvas cs)
+        //{
+        //    super.onDraw(cs);
+
+        //    for (int i = 0; i < BALL_NUMBER; i++)
+        //    {
+        //        Circle cl = circle.get(i);              //リストから円を取り出す
+
+        //        paint.setColor(Color.rgb(cl.r, cl.g, cl.b));
+        //        paint.setStyle(Paint.Style.FILL);
+        //        cs.drawCircle(cl.x, cl.y, cl.s, paint);    //円の描画
+        //    }
+        //}
+    }
+}
---
+++ DrawPointsMultiCanvas/DrawPointsMultiCanvas/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace DrawPointsMultiCanvas
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ResourceLoader resource;
+        List<DrawPoint> DrawPointList;
+        int Count;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            resource = ResourceLoader.GetForCurrentView();
+            Pbtn_Run.Content = resource.GetString("IDC_RUN");
+
+            DrawPointList = new List<DrawPoint>();
+            Count = 0;
+        }
+
+        private void Pbtn_Run_Click(object sender, RoutedEventArgs e)
+        {
+            DrawPoint drawPoints = new DrawPoint();
+            DrawPointList.Add(drawPoints);
+
+            drawPoints.Start();
+
+            Count++;
+        }
+    }
+}
