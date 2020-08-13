36b1ecce2bad267c3c134b3d7e455f7796439111
Tsuyoshi <togura55@gmail.com>
2020-08-13 09:40:26 +0900

    1st check-in

---
+++ WinInkSample/WinInkSample/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace WinInkSample
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Initialize the InkCanvas
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+ //           var resource = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView();
+ //           this.TextBlock_DescriptionTitle.Text = resource.GetString("IDS_DESC_TITLE");
+ //           this.TextBlock_DescriptionContents.Text = resource.GetString("IDS_DESC_CONTENTS_1");
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateCanvasSize(RootGrid, outputGrid, inkCanvas);
+        }
+
+        private static void UpdateCanvasSize(FrameworkElement root, FrameworkElement output, FrameworkElement inkCanvas)
+        {
+            output.Width = root.ActualWidth;
+            output.Height = root.ActualHeight / 2;
+            inkCanvas.Width = root.ActualWidth;
+            inkCanvas.Height = root.ActualHeight / 2;
+        }
+    }
+}
